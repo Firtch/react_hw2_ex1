01590b151bfb3435ce73a7ada2269ddbe6f57e6c
Firtch <firtch@gmail.com>
2021-05-15 08:52:42 +0300

    add Materials

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import { Button, TextField } from '@material-ui/core';
 
-function countN() {
+function countN() {    
   let num = Number.parseInt(document.getElementById('num').value);
   let sum = num * (num + 1) / 2;
   alert('Сумма первых N натуральных чисел: ' + sum);
@@ -12,7 +13,8 @@ function App() {
   return (
     <div>
       <input type="number" id="num" name="num" />
-      <button onClick={countN}>Считать</button>
+      <TextField id="standard-basic" label="Enter number" />
+      <Button onClick={countN} variant="contained" color="primary">Считать</Button>      
     </div>
   );
 }
